fc148ab3611c4044f0504427971e0a57a9f93e5f
kwonja <kownja87@gmail.com>
2023-08-24 02:20:03 +0900

    IP 도메인 지정

--- src/apis/core.ts
+++ src/apis/core.ts
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
 export const BASE_URL = 'http://localhost:5000/python/';
-
+export const MyNotebook = 'http://192.168.100.1/python/';
 //axios instance
 const instance = (url: string) => {
   return axios.create({
     baseURL: url,
     headers: { 'Content-Type': 'multipart/form-data' },
-    withCredentials: true,
   });
 };
 
-export const api = instance(BASE_URL);
+export const api = instance(MyNotebook);
